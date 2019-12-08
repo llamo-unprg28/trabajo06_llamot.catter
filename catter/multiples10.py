87671f715599328a74b36c431686f253f0f1cb29
llamo-unprg28 <57849468+llamo-unprg28@users.noreply.github.com>
2019-12-08 10:29:56 -0500

    CONDICIONAL MULTIPLE10

---
+++ catter/multiples10.py
@@ -0,0 +1,17 @@
+# mostrar la edad e identificar si esta apto para recibir la pension 65
+
+#DECLARACION
+edad=0
+
+import os
+
+#INPUT
+edad=int(os.sys.argv[1])
+
+#PROCESSING
+if (edad<65):
+    print("no recibe pension")
+if (edad>=65 and edad<70):
+    print("recibe pension 65")
+if (edad>=70 and edad<90)
+    print("recibe pension 75")
