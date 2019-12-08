6f755dd11806e0e0761c2047677c125a681f7f1a
llamo-unprg28 <57849468+llamo-unprg28@users.noreply.github.com>
2019-12-08 09:48:16 -0500

    CONDICIONAL DOBLE6

---
+++ llamo/doble6.py
@@ -0,0 +1,18 @@
+# mostrar que el valor sea mayor que otro valor, si ocurre mostrar correcto
+
+#declaracion
+a=0
+
+
+import os
+
+#INPUT
+a=int(os.sys.argv[1])
+
+
+#PROCESSING
+
+if (a>=7):
+    print("correcto")
+else:
+    print("incorrecto")
