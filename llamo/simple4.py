57ca00048d88f614c825f56f8d5b7f34a22c67b8
llamo-unprg28 <57849468+llamo-unprg28@users.noreply.github.com>
2019-12-08 09:36:08 -0500

    CONDICIONAL SIMPLE4

---
+++ llamo/simple4.py
@@ -0,0 +1,19 @@
+#demotrar si un año es bisiesto
+
+
+#declaracion
+fecha=0
+
+import os
+
+#INPUT
+fecha=int(os.sys.argv[1])
+
+#PROCESSING
+
+
+if (fecha%4==0):
+    print("el año, es un año bisiento")
+
+
+
