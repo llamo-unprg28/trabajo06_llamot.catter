614e6725e75e3af2d8af979729410850bc32d82a
llamo-unprg28 <57849468+llamo-unprg28@users.noreply.github.com>
2019-12-08 10:07:08 -0500

    CONDICIONAL SIMPLE10

---
+++ catter/simple10.py
@@ -0,0 +1,13 @@
+# mostrar la edad e identificar si esta apto para recibir la pension 65
+
+#DECLARACION
+edad=0
+
+import os
+
+#INPUT
+edad=int(os.sys.argv[1])
+
+#PROCESSING
+if (edad>65):
+    print("recibe pension")
