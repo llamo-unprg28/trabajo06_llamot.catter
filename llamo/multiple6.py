95fffbd75d70d8d9eb6808f45baf780eb44691c9
llamo-unprg28 <57849468+llamo-unprg28@users.noreply.github.com>
2019-12-08 09:56:17 -0500

    CONDICIONAL MULTIPLE6

---
+++ llamo/multiple6.py
@@ -0,0 +1,22 @@
+# mostrar que el valor sea mayor
+
+#declaracion
+a=0
+
+
+import os
+
+#INPUT
+a=int(os.sys.argv[1])
+
+
+#PROCESSING
+
+if (a>=7 and a<15):
+    print("mayor")
+if (a<7):
+    print("menor")
+if (a>15):
+    print("muy mayor")
+
+
