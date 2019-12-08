03df73f9e3b205dfb45f0009799101bf01c01397
llamo-unprg28 <57849468+llamo-unprg28@users.noreply.github.com>
2019-12-08 10:20:55 -0500

    CONDICIONAL DOBLE9

---
+++ catter/doble9.py
@@ -0,0 +1,18 @@
+# ingresar dos edades
+# si el primero es mayor que el segundo
+
+#DECLARACION
+edad1=0
+edad2=0
+
+import os
+
+#INPUT
+edad1=int(os.sys.argv[1])
+edad2=int(os.sys.argv[2])
+
+#PROCESSING
+if (edad1>edad2):
+    print("la primera edad es mayor que la segunda")
+else:
+    print("la primera edad no es mayor que la segunda")
