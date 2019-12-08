2fc7df7fe2f2d40f8c958d3a4e9e330034fb02c9
llamo-unprg28 <57849468+llamo-unprg28@users.noreply.github.com>
2019-12-08 10:16:25 -0500

    CONDICIONAL DOBLE6

---
+++ catter/doble6.py
@@ -0,0 +1,21 @@
+#Ingresar el peso de 3 personas
+#si la suma supera 300 kg entran a una dieta intensiva
+
+#DECLARACION
+peso1,peso2,peso3,total=0.0,0.0,0.0,0.0
+
+import os
+
+#INPUT
+peso1=float(os.sys.argv[1])
+peso2=float(os.sys.argv[2])
+peso3=float(os.sys.argv[3])
+
+#PROCESSING
+total=peso1+peso2+peso3
+
+if (total>300):
+    print("reciben una dieta intensiva")
+else:
+    print("no reciben dieta intensiva")
+
