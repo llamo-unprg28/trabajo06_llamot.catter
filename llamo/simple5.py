04d2c78a87e2baeb6c5d9238a9c66101dc3f7521
llamo-unprg28 <57849468+llamo-unprg28@users.noreply.github.com>
2019-12-08 09:36:50 -0500

    CONDICIONAL SIMPLE5

---
+++ llamo/simple5.py
@@ -0,0 +1,25 @@
+# resolver la siguiente ecuacion de segundo orden
+
+
+#declaracion
+a=0
+b=0
+c=0
+total=0
+
+
+import os
+
+#INPUT
+numero=int(os.sys.argv[1])
+numero=int(os.sys.argv[1])
+numero=int(os.sys.argv[1])
+
+
+#PROCESSING
+total=b-b + a*c - a
+
+if (total>=0):
+    print("correcto")
+
+
