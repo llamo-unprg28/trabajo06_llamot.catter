f5b6b1830f7ba6c21eb366cf6b7c3713f4745b78
llamo-unprg28 <57849468+llamo-unprg28@users.noreply.github.com>
2019-12-08 09:55:38 -0500

    CONDICIONAL MULTIPLE5

---
+++ llamo/multiple5.py
@@ -0,0 +1,27 @@
+# resolver la siguiente ecuacion de segundo orden
+
+
+#declaracion
+a=0
+b=0
+c=0
+total=0
+
+
+import os
+
+#INPUT
+numero=int(os.sys.argv[1])
+numero=int(os.sys.argv[1])
+numero=int(os.sys.argv[1])
+
+
+#PROCESSING
+total=b*b-4+a*c
+
+if (total<=0):
+    print("incorrecto")
+if (total>0 and total<=30):
+    print("correcto")
+if (total>30):
+    print("muy bueno")
