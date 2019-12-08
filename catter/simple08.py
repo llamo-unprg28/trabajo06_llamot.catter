a01ddef9ad9aed504abc32ec0e057afadee6025f
llamo-unprg28 <57849468+llamo-unprg28@users.noreply.github.com>
2019-12-08 10:05:57 -0500

    CONDICIONAL SIMPLE08

---
+++ catter/simple08.py
@@ -0,0 +1,14 @@
+# dado un numero entero divididos entre 5, si el residuo da 0 mostrar el numero es divisor de 5
+
+#DECLARACION
+numero1=0
+
+import os
+
+#INPUT
+numero1=int(os.sys.argv[1])
+
+#PROCESSING
+if (numero1 % 5 ==0):
+    print("es divisor de 5")
+
