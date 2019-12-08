4758861ea1f7e42a03e6076f18f1f0d9827c3368
llamo-unprg28 <57849468+llamo-unprg28@users.noreply.github.com>
2019-12-08 09:34:47 -0500

    CONDICIONAL SIMPLE2

---
+++ llamo/simple2.py
@@ -0,0 +1,15 @@
+#mostrar si un palillo de fosforo prende esta bueno
+
+#declaracion
+palillo_fosforo=""
+
+import os
+
+#INPUT
+palillo_fosforo=str(os.sys.argv[1])
+
+#PROCESSING
+
+if (palillo_fosforo=="prende"):
+    print("esta bueno")
+
