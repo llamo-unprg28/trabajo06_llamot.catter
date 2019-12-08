5195d75bf0efc1e0af03d2eb4cab70a7660cbf31
llamo-unprg28 <57849468+llamo-unprg28@users.noreply.github.com>
2019-12-08 09:49:11 -0500

    CONDICIONAL DOBLE7

---
+++ llamo/doble7.py
@@ -0,0 +1,19 @@
+# mostrar que una persona sea mayor de edad
+
+#declaracion
+edaad=0
+
+
+import os
+
+#INPUT
+edad=int(os.sys.argv[1])
+
+
+#PROCESSING
+
+if (edad>18):
+    print("es mayor de edad")
+
+else:
+    print(" no es mayor de edad")
