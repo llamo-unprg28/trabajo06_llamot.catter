fa6db82e240da9afc01dc5bf8acf2563413c8783
llamo-unprg28 <57849468+llamo-unprg28@users.noreply.github.com>
2019-12-08 09:47:37 -0500

    CONDICIONAL DOBLE 5

---
+++ llamo/doble5.py
@@ -0,0 +1,25 @@
+# resolver la siguiente ecuacion de segundo orden
+
+
+#declaracion
+a=0
+b=0
+c=0
+total=0
+
+
+import os
+
+#INPUT
+numero=int(os.sys.argv[1])
+numero=int(os.sys.argv[1])
+numero=int(os.sys.argv[1])
+
+
+#PROCESSING
+total=b*b-4+a*c
+
+if (total>=0):
+    print("correcto")
+else:
+    print("incorrecto")
