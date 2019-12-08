6c2b81f7128a4f9484a4b0a09ce6d089fab956c9
llamo-unprg28 <57849468+llamo-unprg28@users.noreply.github.com>
2019-12-08 11:05:14 -0500

    lote bat

---
+++ lote.bat
@@ -0,0 +1,244 @@
+python llamo/simple1.py 25
+python llamo/simple1.py 27
+python llamo/simple1.py 51
+
+python llamo/simple2.py prende
+python llamo/simple2.py no prende
+python llamo/simple2.py parece que prende
+
+python llamo/simple3.py 12 5 4
+python llamo/simple3.py 11 15 14
+python llamo/simple3.py 10 4  7
+
+python llamo/simple4.py 2001
+python llamo/simple4.py 1587
+python llamo/simple4.py 2012
+
+python llamo/simple5.py 2  4 5
+python llamo/simple5.py 5  54 5
+python llamo/simple5.py 1  0  7
+
+python llamo/simple6.py 7
+python llamo/simple6.py 5
+python llamo/simple6.py 9
+
+python llamo/simple7.py 18
+python llamo/simple7.py 16
+python llamo/simple7.py 25
+
+python llamo/simple8.py "jhon" 25 15
+python llamo/simple8.py "maria " 30 15
+python llamo/simple8.py "elias" 40 15
+
+python llamo/simple9.py 36
+python llamo/simple9.py 38
+python llamo/simple9.py 45
+
+python llamo/simple10.py 200
+python llamo/simple10.py 300
+python llamo/simple10.py 600
+
+
+python llamo/doble1.py 25
+python llamo/doble1.py 27
+python llamo/doble1.py 51
+
+python llamo/doble2.py prende
+python llamo/doble2.py no prende
+python llamo/doble2.py parece que prende
+
+python llamo/doble3.py 12 5 4
+python llamo/doble3.py 11 15 14
+python llamo/doble3.py 10 4  7
+
+python llamo/doble4.py 2001
+python llamo/doble4.py 1587
+python llamo/doble4.py 2012
+
+python llamo/doble5.py 2  4 5
+python llamo/doble5.py 5  54 5
+python llamo/doble5.py 1  0  7
+
+python llamo/doble6.py 7
+python llamo/doble6.py 5
+python llamo/doble6.py 9
+
+python llamo/doble7.py 18
+python llamo/doble7.py 16
+python llamo/doble7.py 25
+
+python llamo/doble8.py "jhon" 25 15
+python llamo/doble8.py "jhon" 30 15
+python llamo/doble8.py "jhon" 40 15
+
+python llamo/doble9.py 36
+python llamo/doble9.py 38
+python llamo/doble9.py 45
+
+python llamo/doble10.py 200
+python llamo/doble10.py 300
+python llamo/doble10.py 600
+
+
+python llamo/multiple1.py 25
+python llamo/multiple1.py 27
+python llamo/multiple1.py 51
+
+python llamo/multiple2.py prende
+python llamo/multiple2.py no prende
+python llamo/multiple2.py parece que prende
+
+python llamo/multiple3.py 12 5 4
+python llamo/multiple3.py 11 15 14
+python llamo/multiple3.py 10 4  7
+
+python llamo/multiple4.py 2001
+python llamo/multiple4.py 1587
+python llamo/multiple4.py 2012
+
+python llamo/multiple5.py 2  4 5
+python llamo/multiple5.py 5  54 5
+python llamo/multiple5.py 1  0  7
+
+python llamo/multiple6.py 7
+python llamo/multiple6.py 5
+python llamo/multiple6.py 9
+
+python llamo/multiple7.py 18
+python llamo/multiple7.py 16
+python llamo/multiple7.py 25
+
+python llamo/multiple8.py "jhon" 25 15
+python llamo/multiple8.py "jhon" 30 15
+python llamo/multiple8.py "jhon" 40 15
+
+python llamo/multiple9.py 36
+python llamo/multiple9.py 38
+python llamo/multiple9.py 45
+
+python llamo/multiple10.py 200
+python llamo/multiple10.py 300
+python llamo/multiple10.py 600
+
+
+
+
+python catter/simple01.py "jhon" 4000
+python catter/simple01.py "carla" 400
+python catter/simple01.py "teffy" 5000
+
+python catter/simple02.py 11 11 11
+python catter/simple02.py 12 9 5
+python catter/simple02.py 16 17 18
+
+python catter/simple03.py "verde"
+python catter/simple03.py "amarillo"
+python catter/simple03.py "rojo"
+
+python catter/simple04.py "juan" 200
+python catter/simple04.py "romero" 300
+python catter/simple04.py "sandro" 100
+
+python catter/simple05.py "gonzales"
+python catter/simple05.py "martinez"
+python catter/simple05.py "rosalez"
+
+python catter/simple06.py 110 100 80
+python catter/simple06.py 120 150 80
+python catter/simple06.py  80  90 100
+
+python catter/simple07.py "manuel" 1.60
+python catter/simple07.py "valencia" 1.64
+python catter/simple07.py "alejandra" 1.70
+
+python catter/simple08.py 25
+python catter/simple08.py 23
+python catter/simple08.py 90
+
+python catter/simple09.py 12 34
+python catter/simple09.py 19 15
+python catter/simple09.py 34 28
+
+python catter/simple10.py 56
+python catter/simple10.py 66
+python catter/simple10.py 77
+
+python catter/doble1.py "rafael" 4000
+python catter/doble1.py "micaela" 3000
+python catter/doble1.py "jilberto" 5000
+
+python catter/doble2.py 13 15 16
+python catter/doble2.py 11 11 11
+python catter/doble2.py 10 8  15
+
+python catter/doble3.py verde
+python catter/doble3.py amarillo
+python catter/doble3.py rojo
+
+python catter/doble4.py pedro 70
+python catter/doble4.py pedro 100
+python catter/doble4.py pedro 300
+
+python catter/doble5.py  gonzales
+python catter/doble5.py  flores
+python catter/doble5.py  tineo
+
+python catter/doble6.py  130 140 120
+python catter/doble6.py  90   70 140
+python catter/doble6.py  100 120 70
+
+python catter/doble7.py jose 1.66
+python catter/doble7.py jose 1.60
+python catter/doble7.py jose 1.75
+
+python catter/doble8.py 56
+python catter/doble8.py 30
+python catter/doble8.py 25
+
+python catter/doble9.py 34 23
+python catter/doble9.py 66 23
+python catter/doble9.py 23 23
+
+python catter/doble10.py 64
+python catter/doble10.py 68
+python catter/doble10.py 78
+
+python catter/multiples1.py "rafael" 4000
+python catter/multiples1.py "isrrael" 300
+python catter/multiples1.py "ignacio" 5000
+
+python catter/multiples2.py 13 15 16
+python catter/multiples2.py 11 11 11
+python catter/multiples2.py 10 8  15
+
+python catter/multiples3.py verde
+python catter/multiples3.py amarillo
+python catter/multiples3.py rojo
+
+python catter/multiples4.py pedro 70
+python catter/multiples4.py juan 100
+python catter/multiples4.py ricardo 300
+
+python catter/multiples5.py  gonzales
+python catter/multiples5.py  flores
+python catter/multiples5.py  tineo
+
+python catter/multiples6.py  130 140 120
+python catter/multiples6.py  90   70 140
+python catter/multiples6.py  100 120 70
+
+python catter/multiples7.py jose 1.66
+python catter/multiples7.py jose 1.60
+python catter/multiples7.py jose 1.75
+
+python catter/multiples8.py 56
+python catter/multiples8.py 30
+python catter/multiples8.py 25
+
+python catter/multiples9.py 34 23
+python catter/multiples9.py 66 23
+python catter/multiples9.py 23 23
+
+python catter/multiples10.py 64
+python catter/multiples10.py 68
+python catter/multiples10.py 78
