b276e794309f5033edb9928049d15338a8a717a4
llamo-unprg28 <57849468+llamo-unprg28@users.noreply.github.com>
2019-12-08 09:45:57 -0500

    CONDICIONAL DOBLE3

---
+++ llamo/doble3.py
@@ -0,0 +1,24 @@
+#ingresar 3 notas de un alumno
+#si el promedio es mayor o igual a 7 mostrar un msje "promocionado"
+
+#declaracion
+nota1=0
+nota2=0
+nota3=0
+prom=0
+
+import os
+
+#INPUT
+nota1=int(os.sys.argv[1])
+nota2=int(os.sys.argv[2])
+nota3=int(os.sys.argv[3])
+
+#PROCESSING
+prom=(nota1+nota2+nota3)/3
+
+
+if (prom>=7):
+    print("promocionado")
+else:
+    print("no esta promocionado")
