e1ab2c23eaf03c3f5b98efcccffbf775a63f17f7
llamo-unprg28 <57849468+llamo-unprg28@users.noreply.github.com>
2019-12-08 10:03:11 -0500

    CONDICIONAL SIMPLE04

---
+++ catter/simple04.py
@@ -0,0 +1,18 @@
+#si la compra es mayor a 100 soles optengo un descuento del 10%
+
+
+#DECLARACION
+nombre_del_cliente=""
+monto=0.0
+
+import os
+
+#INPUT
+nombre_del_cliente=str(os.sys.argv[1])
+monto=float(os.sys.argv[2])
+
+#PROCESSING
+tiene_descuento=(monto>100)
+
+if (monto>100):
+    print("tiene 10% de descuento")
