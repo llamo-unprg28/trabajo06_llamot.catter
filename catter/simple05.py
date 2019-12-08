273d13109259be93ea1d30a42850b65dc2db7397
llamo-unprg28 <57849468+llamo-unprg28@users.noreply.github.com>
2019-12-08 10:03:49 -0500

    CONDICIONAL SIMPLE05

---
+++ catter/simple05.py
@@ -0,0 +1,14 @@
+#motrar si el cliente se apellida gonzales participa del sorteo de un carro
+
+#DECLARACION
+apellido_del_cliente=""
+
+import os
+
+#INPUT
+apellido_del_cliente=str(os.sys.argv[1])
+
+#PROCESSING
+if (apellido_del_cliente=="gonzales"):
+    print("participa del sorteo")
+
